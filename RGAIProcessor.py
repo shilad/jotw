2b58bc58b0ce1ae59cc30c0c9aa79f6f1f086b84
mjulstro <mjulstro@macalester.edu>
2017-07-18 10:38:46 -0500

    Cleaned up code
    Removed commented-out sections, lists that aren't used anymore, and a return statement that didn't actually do anything

--- RGAIProcessor.py
+++ RGAIProcessor.py
@@ -12,11 +12,7 @@ class RGAIProcessor():
         self.df = self.df.sort_values(self.df.columns[0])
         self.df = self.df.reindex()  # on second thought, not sure this line is necessary
 
-        # create a dictionary to store differences and a dict for invalid input (may delete these later)
-        self.valid_mismatches = {}  # may delete this later
-        self.invalid_pairs = {}  # may delete this later too
-
-        # create lists to store numbers of mismatches later
+        # create dictionaries to store mismatches
         self.race_data = {'black': {'black': [], 'white': [], 'hispanic': [], 'east asian': [], 'south asian': [],
                                     'native american': [], 'middle eastern': [], 'not sure': [], 'other': [], 'nan': []},
                           'white': {'black': [], 'white': [], 'hispanic': [], 'east asian': [], 'south asian': [],
@@ -68,8 +64,8 @@ class RGAIProcessor():
                                 'nan': {'sax': [], 'trumpet': [], 'trombone': [], 'piano': [], 'voice': [], 'guitar': [],
                                         'bass': [], 'drums': [], 'other': [], 'nan': []}}
 
-        # creates a list of age pairs that are close enough together for disagreement not to matter
-        self.age_pairs = [['child', 'young adult'], ['middle-aged', 'young adult'], ['elder', 'middle-aged']]
+        # # creates a list of age pairs that are close enough together for disagreement not to matter
+        # self.age_pairs = [['child', 'young adult'], ['middle-aged', 'young adult'], ['elder', 'middle-aged']]
 
         # creates a list of URLs deleted for having invalid input from both people, therefore probably not being videos
         self.deleted_urls = []
@@ -78,22 +74,10 @@ class RGAIProcessor():
     def compare_value(self, valueA, valueB, dictionary, value_options, url):
         if valueA in value_options and valueB in value_options:
             dictionary[valueA][valueB].append(url)
-            # can add a line here later to keep track of which URLs these are
         else:
             self.invalid_input.append(url)
         if valueA == "nan" and valueB == "nan":
             self.deleted_urls.append(url)
-        # if valueA != valueB:
-        #     if valueA in value_options and valueB in value_options:
-        #         if url not in self.valid_mismatches.keys():
-        #             self.valid_mismatches[url] = {value: [valueA, valueB]}
-        #         else:
-        #             self.valid_mismatches[url][value] = [valueA, valueB]
-        #     else:
-        #         if url not in self.invalid_pairs.keys():
-        #             self.invalid_pairs[url] = {value: [valueA, valueB]}
-        #         else:
-        #             self.invalid_pairs[url][value] = [valueA, valueB]
 
     def compare_rows(self):
         # iterate over even-numbered rows in the dataframe and compare each video to itself
@@ -124,43 +108,6 @@ class RGAIProcessor():
                 self.compare_value(rowA_gender, rowB_gender, self.gender_data, self.gender_data.keys(), rowA_url)
                 self.compare_value(rowA_age, rowB_age, self.age_data, self.age_data.keys(), rowA_url)
                 self.compare_value(rowA_instrument, rowB_instrument, self.instrument_data, self.instrument_data.keys(), rowA_url)
-                    # if rowA_race in self.race_options and rowB_race in self.race_options:
-                    #     if rowA_url not in self.valid_mismatches.keys():
-                    #         self.valid_mismatches[rowA_url] = {'race': [rowA_race, rowB_race]}
-                    #     else:
-                    #         self.valid_mismatches[rowA_url]['race'] = [rowA_race, rowB_race]
-                    # else:
-                    #     if rowA_url not in self.invalid_pairs.keys():
-                    #         self.invalid_pairs[rowA_url] = {'race': [rowA_race, rowB_race]}
-                    #     else:
-                    #         self.invalid_pairs[rowA_url]['race'] = [rowA_race, rowB_race]
-
-                # self.compare_value(rowA_race, rowB_race, 'race', self.race_options, rowA_url)
-                # self.compare_value(rowA_gender, rowB_gender, 'gender', self.gender_options, rowA_url)
-                # self.compare_value(rowA_instrument, rowB_instrument, 'instrument', self.instrument_options, rowA_url)
-
-                # ages = [rowA_age, rowB_age]
-                # ages.sort()
-                # if ages not in self.age_pairs:  # ignores mismatches where age results are very close
-                #     self.compare_value(rowA_age, rowB_age, 'age', self.age_data.keys(), rowA_url)
-
-        return self.valid_mismatches
-
-    # def count_value_mismatches(self, value, input_dictionary):
-    #     count = 0
-    #     differences = {}  # number of times people have disagreed on this value in a specific way
-    #     for key in input_dictionary.keys():
-    #         if value in input_dictionary[key].keys():
-    #             count += 1
-    #             curr_difference = input_dictionary[key][value]
-    #             curr_difference.sort()  # this ensures that ['young adult', 'elder'] and ['elder', 'young adult'] are
-    #             # recognized as being the same kind of disagreement
-    #             curr_difference = tuple(curr_difference)
-    #             if curr_difference not in differences:
-    #                 differences[curr_difference] = 1
-    #             else:
-    #                 differences[curr_difference] += 1
-    #     return [count, differences]
 
     def fill_count_dict(self, input_dict, output_dict):
         for row_key in input_dict.keys():
@@ -189,38 +136,6 @@ class RGAIProcessor():
 
         print len(self.deleted_urls), "URLs had one or more field in which both inputs were NaN."
         print "People entered non-options for", len(self.invalid_input), "URLs. They were:", self.invalid_input
-
-        # print "There are", len(self.valid_mismatches.keys()), "valid, mismatched pairs and",\
-        #       len(self.invalid_pairs.keys()), "pairs with at least one invalid input."
-        # print ""
-        #
-        # valid_race_counts = self.count_value_mismatches('race', self.valid_mismatches)
-        # valid_gender_counts = self.count_value_mismatches('gender', self.valid_mismatches)
-        # valid_age_counts = self.count_value_mismatches('age', self.valid_mismatches)
-        # valid_instrument_counts = self.count_value_mismatches('instrument', self.valid_mismatches)
-        #
-        # invalid_race_counts = self.count_value_mismatches('race', self.invalid_pairs)
-        # invalid_gender_counts = self.count_value_mismatches('gender', self.invalid_pairs)
-        # invalid_age_counts = self.count_value_mismatches('age', self.invalid_pairs)
-        # invalid_instrument_counts = self.count_value_mismatches('instrument', self.invalid_pairs)
-        #
-        # print "Of the valid, mismatched pairs,", valid_race_counts[0], "disagreed on the performer's race,",\
-        #       valid_gender_counts[0],"disagreed on gender,", valid_age_counts[0],"disagreed significantly on age, and",\
-        #       valid_instrument_counts[0] ,"disagreed on instrument."
-        # print "Race:", valid_race_counts[1]
-        # print "Gender:", valid_gender_counts[1]
-        # print "Age:", valid_age_counts[1]
-        # print "Instrument:", valid_instrument_counts[1]
-        # print ""
-        # print "Of the pairs containing invalid inputs, there were", invalid_race_counts[0],"pairs for race,",\
-        #       invalid_gender_counts[0],"pairs for gender,", invalid_age_counts[0],"pairs for age, and",\
-        #       invalid_instrument_counts[0],"pairs for instrument."
-        # print "Race:", invalid_race_counts[1]
-        # print "Gender:", invalid_gender_counts[1]
-        # print "Age:", invalid_age_counts[1]
-        # print "Instrument:", invalid_instrument_counts[1]
-
-
 
 processor = RGAIProcessor('//Users/sen/Desktop/RGAI.csv')
 processor.compare_rows()
